ff73d15392fbf277a8174aaf414b8a39237176f1
alchemistlowkey <alchemistlowkey@gmail.com>
2025-04-12 07:28:41 +0100

    added meta tags to dashboard page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
 	import Login from '$lib/components/Login.svelte';
 
-	export let title = 'FleetSync: Manage Drivers, Vehicles & Trips';
+	export let title = 'FleetSync';
 	export let description =
 		"Monitor and manage drivers, vehicles, and trips with FleetSync's intuitive dashboard. Track active trips, assign vehicles, and optimize logistics efficiently.";
 </script>
@@ -9,13 +9,13 @@
 <svelte:head>
 	<title>{title}</title>
 	<meta name="description" content={description} />
-	<link rel="canonical" href="https://workplace-pi.vercel.app" />
-	<meta name="image" content="https://workplace-pi.vercel.app/logo.png" />
+	<link rel="canonical" href="https://fleet-management-inky.vercel.app/" />
+	<meta name="image" content="https://fleet-management-inky.vercel.app/logo.png" />
 	<meta property="og:title" content={title} />
 	<meta property="og:description" content={description} />
-	<meta property="og:url" content="https://workplace-pi.vercel.app" />
+	<meta property="og:url" content="https://fleet-management-inky.vercel.app/" />
 	<meta property="og:type" content="website" />
-	<meta property="og:image" content="https://workplace-pi.vercel.app/logo.png" />
+	<meta property="og:image" content="https://fleet-management-inky.vercel.app/logo.png" />
 </svelte:head>
 
 <Login />
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,5 +1,21 @@
 <script>
 	import Dashboard from '$lib/components/Dashboard.svelte';
+
+	export let title = 'FleetSync: Manage Drivers, Vehicles & Trips';
+	export let description =
+		"Monitor and manage drivers, vehicles, and trips with FleetSync's intuitive dashboard. Track active trips, assign vehicles, and optimize logistics efficiently.";
 </script>
+
+<svelte:head>
+	<title>{title}</title>
+	<meta name="description" content={description} />
+	<link rel="canonical" href="https://fleet-management-inky.vercel.app/dashboard" />
+	<meta name="image" content="https://fleet-management-inky.vercel.app/logo.png" />
+	<meta property="og:title" content={title} />
+	<meta property="og:description" content={description} />
+	<meta property="og:url" content="https://fleet-management-inky.vercel.app/dashboard" />
+	<meta property="og:type" content="website" />
+	<meta property="og:image" content="https://fleet-management-inky.vercel.app/logo.png" />
+</svelte:head>
 
 <Dashboard />
